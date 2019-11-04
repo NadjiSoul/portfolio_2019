280b5d43db0948bfcb7cf19bbf05a280b18a4301
Soulaimana Nadji <nadji.soulaimana@gmail.com>
2019-11-04 12:48:53 +0100

    meta viewport

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <html lang="fr" dir="ltr">
 <head>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Portfolio :: NSoulaimana</title>
 	<link rel="icon" type="image/png" href="./img/website/logo.png" />
 	<link rel="stylesheet" type="text/css" href="./css/style.css">
